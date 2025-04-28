5e3a29688cbde8b8eb6747d1482c67e84a924ba6
James-Frown <21122@ctca.co.za>
2025-04-29 00:06:58 +0200

    feat(about): made better

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,10 @@
-import CTASection from "@/components/cta/cta_section";
 import Image from "next/image";
+
 import ProfileImage3 from "../../assets/images/ProfileImg_3.jpg";
+const Resume = "/resume/Resume.pdf";
+
+import Button from "@/components/button/Button";
+import CTASection from "@/components/cta/cta_section";
 
 export default function ABOUT() {
   return (
@@ -12,20 +16,32 @@ export default function ABOUT() {
             width={300}
             height={300}
             alt="Profile Image 2"
-            className="h-[50%] object-contain rounded-[30px] p-4"
+            className="object-cover hidden sm:inline-block rounded-[30px] p-4"
           />
-          <div className="text-black flex flex-col items-center justify-center w-[80%] h-[80%] bg-white rounded-[30px] p-4 shadow-lg">
-            <h1 className="text-3xl font-bold mb-4">About Me</h1>
+          <Image
+            src={ProfileImage3}
+            width={200}
+            height={200}
+            alt="Profile Image 2"
+            className="object-cover rounded-[100%] sm:hidden p-4"
+          />
+          <div className="text-gray-700 flex flex-col items-center text-center justify-center gap-8 w-[100%] h-[100%] sm:w-[80%] sm:h-[80%] p-4">
             <p>
-              As a dedicated webmaster and developer at BankingAcumen.com, and
-              an intern at The Good People Data Company, I am actively honing my
-              technical skills and building expertise in the dynamic tech
-              landscape. A passionate tech enthusiast with an entrepreneurial
-              mindset, I am driven to innovate, solve complex problems, and make
-              a lasting impact through my work. My commitment to continuous
-              learning and willingness to embrace calculated risks empower me to
-              push boundaries and exceed expectations, fueled by a relentless
-              curiosity and a determination to succeed.
+              As a dedicated{" "}
+              <span className="font-semibold text-gray-700">Webmaster</span> and{" "}
+              <span className="font-semibold text-gray-700">Developer</span> at{" "}
+              <span className="font-bold text-black">Banking Acumen</span>, and
+              an <span className="font-semibold text-gray-700">Intern</span> at{" "}
+              <span className="font-bold text-black">
+                The Good People Data Company
+              </span>
+              , I am actively honing my technical skills and building expertise
+              in the dynamic tech landscape. A passionate tech enthusiast with
+              an entrepreneurial mindset, I am driven to innovate, solve complex
+              problems, and make a lasting impact through my work. My commitment
+              to continuous learning and willingness to embrace calculated risks
+              empower me to push boundaries and exceed expectations, fueled by a
+              relentless curiosity and a determination to succeed.
             </p>
             <p>
               I excel as both an independent self-starter and a collaborative
@@ -42,8 +58,11 @@ export default function ABOUT() {
               grow and evolve as a professional. With a passion for innovation,
               a collaborative spirit, and an unyielding work ethic, I am
               confident in my ability to make significant contributions to any
-              organization I join
+              organization I join.
             </p>
+            <Button id="Resume_Button" src={Resume}>
+              Resume
+            </Button>
           </div>
         </CTASection>
       </main>
