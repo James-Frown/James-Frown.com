5b0dbc18e34924e1ac2411cb7a365067b7c88dc3
James-Frown <21122@ctca.co.za>
2025-04-28 16:11:12 +0200

    feat(landing): add landing touches

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,15 @@ export default function HOME() {
       <main className="flex flex-row flex-wrap items-center justify-around h-[90vh] bg-gray-100">
         <Image
           src={ProfileImage2}
-          width={150}
-          height={150}
+          width={200}
+          height={200}
           alt="Profile Image"
-          className="rounded-[10px] object-cover p-4"
+          className="rounded-[30px] object-cover p-4"
         />
         <div className="flex flex-col items-center justify-center p-4">
           <h1 className="text-black text-4xl font-bold">Hello!</h1>
           <p className="text-black text-lg">Welcome to my portfolio!</p>
-          <p className="text-black text-md p-2 hover:text-blue-500">
+          <p className="text-blue-500 font-bold text-md p-2 opacity-0 md:opacity-100">
             <Link
               href="/about"
               className="hover:border-b-blue-500 border-b-2 border-transparent transition duration-300 ease-in-out p-2"
@@ -24,6 +24,11 @@ export default function HOME() {
               Learn More
             </Link>
           </p>
+          <button className="bg-black text-white text-md p-2 rounded md:hidden">
+            <Link href="/about" className="p-2">
+              Learn More
+            </Link>
+          </button>
         </div>
       </main>
     </>
